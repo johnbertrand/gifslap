b7c15e0d5820f0bed9991b63dd58760273dbfe65
Donald Hanson <don@gridwalk.net>
2014-07-07 20:24:40 -0700

    starting on MIDI mapping tool

--- core/controls/control_registry.js
+++ core/controls/control_registry.js
@@ -1,17 +1,52 @@
 //control registry
 
-var control_registry = {
-  single_height : {
-    name: "Single Height",
-    type: "knob",
-    midi_response: function( value ){
-      images.height = value * 8;
+// connect this to an interface for mapping MIDI
+
+var controls = {
+  
+  knobs : {
+
+    single_height : {
+      name: "Single Height",
+      change: function( value ){
+        images.height = value*8;
+        if(images.height<10){images.height=10}
+      }
+      cursor: [false,false], // x,y 
+      midi_control_id: null 
     },
-    basic_response: function( cursor ){
-      images.height = cursor * 30;
+
+    single_height : {
+      name: "Single Width",
+      change: function( value ){
+        images.width = value*14;
+        if(images.width<10){images.width=10}
+      }
+      cursor: [false,false], // x,y 
+      midi_control_id: null 
     }
-    // activation_key: null, 
-    // midi_control_id: null 
+
+  },
+  buttons : {
+
+    next_image : {
+      name: "Next Image",
+      press: function(){
+        images.next();
+      },
+      keystroke: null,
+      midi_control_id: null
+    },
+
+    next_background : {
+      name: "Next Background",
+      press: function(){
+        bg_mod.next();
+      },
+      keystroke: null,
+      midi_control_id: null
+    }
+
   }
 
 }
